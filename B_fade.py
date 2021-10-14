974f84ce772c876ef395be38d9a415f3b5413c83
BNS-Ichthus <89841683+BNS-Ichthus@users.noreply.github.com>
2021-10-14 19:20:51 +0200

    Smoothen fading effect

--- B_fade.py
+++ B_fade.py
@@ -7,9 +7,9 @@ ledPin = Pin(4, Pin.OUT)      # ledpin object maken
 led = PWM(ledPin, freq=1000)  # PWM toevoegen aan de pin
 
 def pulse(pin, t):
-  for i in range(20):
-    pin.duty(int(sin(i / 10 * pi) * 500 + 500)) # pin PWM-belasting aanzetten
+  for i in range(40):
+    pin.duty(int(sin(i / 20 * pi) * 500 + 500)) # pin PWM-belasting instellen
     sleep_ms(t)               # slapen in milliseconden
           
 while True:
-  pulse(led, 50)              # continu faden
+  pulse(led, 25)              # continu faden
